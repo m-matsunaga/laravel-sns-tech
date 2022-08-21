9ee351507d3d6a19106d9b00c25a781b3306b5f7
matsunaga.miori <miori.matsunaga26@gmail.com>
2022-08-21 23:00:28 +0900

    記事更新機能・記事削除機能　追加

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -20,8 +20,7 @@ class ArticleController extends Controller
     }
 
     public function store(ArticleRequest $request,Article $article){
-        $article->title = $request->title;
-        $article->body = $request->body;
+        $article->fill($request->all());
         $article->user_id = $request->user()->id;
         $article->save();
         return redirect()->route('articles.index');
@@ -29,5 +28,16 @@ class ArticleController extends Controller
 
     public function edit(Article $article){
         return view('articles.edit',['article' => $article]);
+    }
+
+    public function update(ArticleRequest $request,Article $article){
+        $article->fill($request->all());
+        $article->save();
+        return redirect()->route('articles.index');
+    }
+
+    public function destroy(Article $article){
+        $article->delete();
+        return redirect()->route('articles.index');
     }
 }

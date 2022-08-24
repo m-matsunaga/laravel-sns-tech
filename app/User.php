84b50b76aff6c3a639e30c4d425c1764e0a8afec
matsunaga.miori <miori.matsunaga26@gmail.com>
2022-08-24 11:08:45 +0900

    パスワード再設定メール送信機能　追加

--- app/User.php
+++ app/User.php
@@ -2,6 +2,10 @@
 
 namespace App;
 
+// パスワード変更メール //
+use App\Mail\BareMail;
+use App\Notifications\PasswordResetNotification;
+
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -36,4 +40,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendPasswordResetNotification($token){
+        $this->notify(new PasswordResetNotification($token, new BareMail()));
+    }
 }

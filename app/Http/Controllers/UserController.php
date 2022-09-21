500dac95e5a7431fb1a413943b9f31d1b6e6c5ee
matsunaga.miori <miori.matsunaga26@gmail.com>
2022-09-21 09:24:11 +0900

    N+1問題（SQL改善）

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,28 +8,28 @@ use App\User;
 class UserController extends Controller
 {
     public function show(string $name){
-        $user = User::where('name',$name)->first();
+        $user = User::where('name',$name)->first()->load(['articles.user', 'articles.likes', 'articles.tags']);
         $articles = $user->articles->sortByDesc('created_at');
 
         return view('users.show', ['user' => $user,'articles' => $articles,]);
     }
 
     public function likes(string $name){
-        $user = User::where('name',$name)->first();
+        $user = User::where('name',$name)->first()->load(['likes.user', 'likes.likes', 'likes.tags']);
         $articles = $user->likes->sortByDesc('created_at');
 
         return view('users.likes', ['user' => $user,'articles' => $articles,]);
     }
 
     public function followings(string $name){
-        $user = User::where('name',$name)->first();
+        $user = User::where('name',$name)->first()->load('followings.followers');
         $followings = $user->followings->sortByDesc('created_at');
 
         return view('users.followings', ['user' => $user,'followings' => $followings,]);
     }
 
     public function followers(string $name){
-        $user = User::where('name',$name)->first();
+        $user = User::where('name',$name)->first()->load('followers.followers');
         $followers = $user->followers->sortByDesc('created_at');
 
         return view('users.followers', ['user' => $user,'followers' => $followers,]);
